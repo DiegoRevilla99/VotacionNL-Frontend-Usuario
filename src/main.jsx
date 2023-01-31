96c62545b4722f8a8d02fd54dfa734c7fbbeecec
Diego Revilla <string.diego.revilla@gmail.com>
2023-01-30 18:29:20 -0600

    Añadiendo hash

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		{/* <BrowserRouter> */}
+		<HashRouter>
 			<Provider store={store}>
 				<App />
 			</Provider>
-		</BrowserRouter>
+		</HashRouter>
+		{/* </BrowserRouter> */}
 	</React.StrictMode>
 );
